714cc79267c2842fad1ff14917072fcca669563f
Peter S <tvojmail@nieco.sk>
2026-02-14 23:19:03 +0100

    Import coso-nevedel template into coso-template/template-root

--- packages/coso-template/template-root/next.config.ts
+++ packages/coso-template/template-root/next.config.ts
@@ -1,16 +1,7 @@
-import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["coso-nevedel-core"],
-  webpack: (config) => {
-    config.resolve = config.resolve ?? {};
-    config.resolve.alias = {
-      ...(config.resolve.alias ?? {}),
-      "coso-nevedel-core": path.resolve(__dirname, "../../packages/coso-nevedel-core/src/index.ts"),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
