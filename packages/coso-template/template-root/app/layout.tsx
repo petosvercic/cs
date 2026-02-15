2a56b7775159b2981b4397e56dacdd2b9444d049
Peter Šverčič <narakanakaru@gmail.com>
2026-02-15 02:26:47 +0100

    Update layout.tsx

--- packages/coso-template/template-root/app/layout.tsx
+++ packages/coso-template/template-root/app/layout.tsx
@@ -6,7 +6,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="relative min-h-screen bg-[url('/brand/bg.png')] bg-cover bg-center bg-no-repeat">
         <div className="pointer-events-none fixed inset-0 bg-black/35" aria-hidden="true" />
         <div className="relative z-10">{children}</div>
-      </body>
+         <body style={{ backgroundImage: "url(/brand/bg.png)", backgroundSize: "cover", backgroundPosition: "center" }}>
+       <img src="/brand/logo.png" alt="Brand" style={{ height: 48, width: "auto" }} />
     </html>
   );
 }
