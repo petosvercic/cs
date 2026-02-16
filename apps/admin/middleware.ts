5c6d4db2cae7025daa8b294059ede8db7f893655
Peter Šverčič <narakanakaru@gmail.com>
2026-02-16 21:13:59 +0100

    Update middleware.ts

--- apps/admin/middleware.ts
+++ apps/admin/middleware.ts
@@ -18,5 +18,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image).*)"]
+  matcher: ["/((?!_next|favicon.ico|api).*)"],
 };
