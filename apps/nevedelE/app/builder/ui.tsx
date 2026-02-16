ebb7f9a5b38e36168690fc9156a401429fe9198e
Peter S <tvojmail@nieco.sk>
2026-02-16 18:05:43 +0100

    fix(nevedelE): wire builder ui from factory-ui package

--- apps/nevedelE/app/builder/ui.tsx
+++ apps/nevedelE/app/builder/ui.tsx
@@ -1,12 +1,3 @@
-﻿type Props = {
-  initialEditions?: unknown;
-};
+﻿import { NevedelEBuilderUi } from "<PACKAGE_NAME>";
 
-export default function BuilderUi(_props: Props) {
-  return (
-    <main style={{ padding: 16 }}>
-      <h1>Builder</h1>
-      <p>Builder UI module missing. Restore or rewire to the correct component.</p>
-    </main>
-  );
-}
+export default NevedelEBuilderUi;
